4ae7737b6e6804018d0212b146dbf81daf815a1d
mellyday <76913671+Mellyday@users.noreply.github.com>
2022-07-05 22:51:13 +0700

    Refactor This version of the code is much less noisier. No default dict, no manual counting of length.

--- Read Quality Control/task/control.py
+++ Read Quality Control/task/control.py
@@ -1,18 +1,11 @@
 # write your code here
-from collections import defaultdict
-length_dict = defaultdict(lambda *_: 0)
-
 filename = input()
-reads_num = 0
-with open(filename, encoding="utf-8") as file:
-    lines = [line.strip() for line in file.readlines()]
-    for line in lines[1::4]:
-        reads_num += 1
-        length = len(line)
-        length_dict[length] = length_dict[length] + 1
-print(f"Reads in the file = {reads_num}:")
-total_length = 0
-for length, num in length_dict.items():
-    total_length += length * num
-    print(f"      with length {length} = {num}")
-print(f"Reads sequence average length = {round(total_length / reads_num)}")
+with open(filename, encoding="utf-8") as FASTQ_FILE:
+    lines = FASTQ_FILE.readlines()
+    lengths = [len(line.strip()) for line in lines[1::4]]
+    amount = len(lengths)
+print(f"Reads in the file = {amount}:")
+for le in set(lengths):
+    print(f"      with length {le} = {lengths.count(le)}")
+print()
+print(f"Reads sequence average length = {round(sum(lengths) / amount)}")
