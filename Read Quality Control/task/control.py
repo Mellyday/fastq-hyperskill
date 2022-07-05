118028fbada06bc2a30e8969dfe56201114f482f
mellyday <76913671+Mellyday@users.noreply.github.com>
2022-07-05 23:45:41 +0700

    Refactor Change to a class-based implementation

--- Read Quality Control/task/control.py
+++ Read Quality Control/task/control.py
@@ -1,11 +1,34 @@
 # write your code here
-filename = input()
-with open(filename, encoding="utf-8") as FASTQ_FILE:
-    lines = FASTQ_FILE.readlines()
-    lengths = [len(line.strip()) for line in lines[1::4]]
-    amount = len(lengths)
-print(f"Reads in the file = {amount}:")
-for le in set(lengths):
-    print(f"      with length {le} = {lengths.count(le)}")
-print()
-print(f"Reads sequence average length = {round(sum(lengths) / amount)}")
+from collections import Counter
+
+
+class DNASequence:
+    def __init__(self, filename):
+        with open(filename) as file:
+            self.lines = [line.strip() for line in file.readlines()]
+
+    @property
+    def dna_sequences(self):
+        sequences = [line for line in self.lines[1::4]]
+        return sequences
+
+    @property
+    def seq_lengths(self):
+        lengths = [len(line) for line in self.dna_sequences]
+        return lengths
+
+
+def main():
+    dna = DNASequence(input())
+    # Need lines, lengths, amount
+    print(f"Reads in the file = {len(dna.dna_sequences)}:")
+    counter = Counter(dna.seq_lengths)
+    for length, count in counter.items():
+        print(f"      with length {length} = {count}")
+    print()
+    avg = sum(dna.seq_lengths) / len(dna.seq_lengths)
+    print(f"Reads sequence average length = {round(avg)}")
+
+
+if __name__ == "__main__":
+    main()
